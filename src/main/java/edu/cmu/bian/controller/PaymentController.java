0ff22d8480abe0078a1fd8e8ac2c917411da54ad
Mark Grobaker <Mark Grobaker>
2017-03-31 18:10:26 +0100

    fri 3.31

--- src/main/java/edu/cmu/bian/controller/PaymentController.java
+++ src/main/java/edu/cmu/bian/controller/PaymentController.java
@@ -4,9 +4,13 @@ import edu.cmu.bian.model.Account;
 import edu.cmu.bian.model.CompositeObject;
 import edu.cmu.bian.model.PartyResponse;
 import edu.cmu.bian.model.PaymentModel;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import javax.net.ssl.HttpsURLConnection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -30,4 +34,36 @@ public class PaymentController {
     }
 
 
+    @GetMapping("/card/{cardNumber}")
+    public String getCardData(@PathVariable String cardNumber) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://apimanager.pncapix.com:8243/smartbank-card/1.0.0/1.0.0/card/{cardNumber}";
+     HttpsURLConnection.setDefaultHostnameVerifier ((hostname, session) -> true);
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("cardNumber", 1);
+
+        Map<String, Object> headers = new HashMap<>();
+
+        headers.put("accept", "application/json");
+        headers.put("authorization", "Bearer ce0fe2cf-2dc0-3e96-b489-5806070265a0");
+//        headers.put("cache-control", "no-cache");
+//        headers.put("postman-token", "1b31b01a-9c73-4d47-092b-9940c9e0da98");
+
+        HttpEntity httpEntity = new HttpEntity(headers);
+
+        ResponseEntity<String> partyResponseEntity =
+                restTemplate.exchange(url, HttpMethod.GET, httpEntity, String.class, paramMap);
+        String partyResponse = partyResponseEntity.getBody();
+
+
+
+//        PartyResponse partyResponse = restTemplate.getForObject(url, PartyResponse.class, paramMap);
+//        System.out.println("party response:" + partyResponse);
+//
+//        long partyId = partyResponse.getContent().get(0).getPartyId();
+//        System.out.println("Party ID: " + partyId);
+
+        System.out.println("partyResponse: " + partyResponse);
+        return partyResponse;
+    }
 }
